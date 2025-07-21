3b6faeddbc81d204b9c8e473746b12b28828ca75
Muzzu153 <muzzusyed153@gmail.com>
2025-07-22 00:11:06 +0530

    Set the layout for routes used to navigate
    Make a layout component in App.jsx and the routes in main.jsx using react-router-dom data mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import MyAppointments from "./pages/MyAppointments";
+import MyProfile from "./pages/MyProfile.jsx";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Appointment from "./pages/Appointment";
+
+let route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "my-appointments", element: <MyAppointments /> },
+      { path: "my-profile", element: <MyProfile /> },
+      { path: "appointments/:docId", element: <Appointment /> },
+      {
+        path: "doctors",
+        element: <Doctors />,
+        children: [
+          { index: true, element: <Doctors /> },
+          { path: ":speciality", element: <Doctors /> },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="mx-4 sm:mx-[10%]">
+        <RouterProvider router={route} />
+    </div>
+  </StrictMode>
+);

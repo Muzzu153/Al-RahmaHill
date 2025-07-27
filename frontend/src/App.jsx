ee9bb06cc6f736c1e84b73a3ba8550b690a0555e
Muzzu153 <muzzusyed153@gmail.com>
2025-07-27 18:38:36 +0530

    Add signup and login modal in layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LoginModal from "./components/LoginModal";
+import SignupModal from "./components/SighupModal";
+import AppContextProvider from "./context/AppContext";
 
 function Layout() {
   return (
     <>
       <Navbar />
+      <LoginModal />
+      <SignupModal />
       <Outlet />
     </>
   );
@@ -13,7 +18,9 @@ function Layout() {
 const App = () => {
   return (
     <div>
-      <Layout />
+      <AppContextProvider>
+        <Layout />
+      </AppContextProvider>
     </div>
   );
 };

34c13b6bf0b6ee4d9cae09973ed2c78a4d4b1e6b
Muzzu153 <muzzusyed153@gmail.com>
2025-07-27 12:28:57 +0530

    Create App context to be used througout project

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useRef } from "react";
+import { doctors } from "../assets/assets";
+// import {}
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+  const currencySymbol = "$";
+
+  const loginDialogRef = useRef();
+  const signupDialogRef = useRef();
+
+  const registerLoginRef = (ref) => {
+    loginDialogRef.current = ref.current;
+  };
+
+  const registerSignupRef = (ref) => {
+    signupDialogRef.current = ref.current;
+  };
+
+  const openLogin = () => loginDialogRef.current?.open();
+  const openSignup = () => signupDialogRef.current?.open();
+  const closeLogin = () => loginDialogRef.current?.close();
+  const closeSignup = () => signupDialogRef.current?.close();
+
+  const closeAll = () => {
+    loginDialogRef.current?.close();
+    signupDialogRef.current?.close();
+  };
+
+  const value = {
+    doctors,
+    currencySymbol,
+    registerLoginRef,
+    registerSignupRef,
+    openLogin,
+    openSignup,
+    closeLogin,
+    closeSignup,
+    closeAll,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;

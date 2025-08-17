8e9408e3eaee009e742e1682e66b12cd78813621
Muzzu153 <muzzusyed153@gmail.com>
2025-08-17 17:56:18 +0530

    Modify the layout with correct names

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LoginModal from "./components/LoginModal";
 import SignupModal from "./components/SighupModal";
-import AppContextProvider from "./context/AppContext";
+import AppContextProvider, { AppContext } from "./context/AppContext";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 function Layout() {
+  const location = useLocation();
+  const { closeAll } = useContext(AppContext);
+
+  useEffect(() => {
+    closeAll();
+  }, [location]);
+
   return (
     <>
+      <ToastContainer />
       <Navbar />
       <LoginModal />
       <SignupModal />
       <Outlet />
-      <Footer/>
+      <Footer />
     </>
   );
 }
